e89ea47f41e08add411e1ee5fd8c5db4e2c74047
Muronha <Wise.Muronha@standardbank.co.mz>
2025-09-02 16:40:20 +0200

    Doubly linked list initialization

---
+++ double_linked_list/main.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self._length = 0
+
+
+
+    
