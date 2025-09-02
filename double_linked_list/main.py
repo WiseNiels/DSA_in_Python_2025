367cfd2247050adada8e1005e93d4858eca1d0d3
Muronha <Wise.Muronha@standardbank.co.mz>
2025-09-02 17:00:17 +0200

    adicionar no inicio da lista

--- double_linked_list/main.py
+++ double_linked_list/main.py
@@ -9,7 +9,8 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
         self._length = 0
-
+        
+    # adiciona no final
     def append(self, data):
         new_node = Node(data)
         if not self.head:
@@ -19,5 +20,16 @@ class DoublyLinkedList:
             self.tail.next = new_node
             new_node.prev = self.tail
             self.tail = new_node
+        self._length += 1
+        return self
+    def prepend(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
         self._length += 1
         return self
